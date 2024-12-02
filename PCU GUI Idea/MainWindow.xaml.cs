70501e17a70edbbcbf8bccf6965c6d8c030a8515
GrozeBogdan <134095563+GrozeBogdan@users.noreply.github.com>
2024-12-02 10:27:18 +0200

    Project Start
    - Added project to Git

---
+++ PCU GUI Idea/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Documents.Spreadsheet.Model;
+
+namespace PCU_GUI_Idea
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = workbook.Worksheets.Add();
+
+            CellSelection selection = worksheet.Cells[1, 1]; //B2 cell 
+            selection.SetValue("Hello RadSpreadProcessing");
+        }
+    }
+}
